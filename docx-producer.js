373de9079f8feffef6d0c9e74c5cf2abbe9e14d7
btboy12 <btboy12@sina.com>
2017-10-25 12:43:00 +0800

    test: doctemplater

---
+++ docx-producer.js
@@ -0,0 +1,84 @@
+var JSZip = require('jszip');
+var Docxtemplater = require('docxtemplater');
+
+var fs = require('fs');
+var path = require('path');
+
+//Load the docx file as a binary
+var content = fs.readFileSync(path.resolve(__dirname, 'input.docx'), 'binary');
+
+var zip = new JSZip(content);
+
+var doc = new Docxtemplater();
+doc.loadZip(zip);
+
+//set the templateVariables
+doc.setData({
+    interface_class: [
+        {
+            key: "1.1",
+            name: "商品接口",
+            interface: [
+                {
+                    key: "1.1.1",
+                    name: "购物车",
+                    reqInfo: [
+                        {
+                            name: "Id",
+                            type: "String",
+                            description: "唯一键值"
+                        }
+                    ],
+                    resInfo: [{
+                        name: "status",
+                        type: "int",
+                        description: "操作结果"
+                    }]
+                }
+            ]
+        },
+        {
+            key: "1.1",
+            name: "商品接口",
+            interface: [
+                {
+                    key: "1.1.1",
+                    name: "购物车",
+                    reqInfo: [
+                        {
+                            name: "Id",
+                            type: "String",
+                            description: "唯一键值"
+                        }
+                    ],
+                    resInfo: [{
+                        name: "status",
+                        type: "int",
+                        description: "操作结果"
+                    }]
+                }
+            ]
+        }
+    ]
+});
+
+try {
+    // render the document
+    doc.render()
+}
+catch (error) {
+    var e = {
+        message: error.message,
+        name: error.name,
+        stack: error.stack,
+        properties: error.properties,
+    }
+    console.log(JSON.stringify({ error: e }));
+    // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+    throw error;
+}
+
+var buf = doc.getZip().generate({ type: 'nodebuffer' });
+
+// buf is a nodejs buffer, you can either write it to a file or do anything else with it.
+fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
