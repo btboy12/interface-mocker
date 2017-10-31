81b41d4b23318f9bae5700c3497533076d7de3d3
btboy12 <btboy12@gmail.com>
2017-10-31 15:59:24 +0800

    自动返回代理服务器端口和地址

--- app/socketio.js
+++ app/socketio.js
@@ -1,4 +1,5 @@
 const { interface } = require("./mapper");
+const proxy_server = require("./proxy_server");
 
 var io;
 exports.create = server => {
@@ -6,6 +7,11 @@ exports.create = server => {
     io.clients
     io.on("connect", socket => {
         console.info("get socket");
+        socket.emit("proxy info", {
+            protocol: "http",
+            port: proxy_server.port,
+            host: "211.83.110.4"
+        })
         interface.findAll({ attributes: ["router", "method"] })
             .then((results) => { socket.emit("interface list", results) });
     });
