d2829bb6be7fc34391952fab206955b6bbb7c4a1
btboy12 <btboy12@gmail.com>
2017-11-01 16:58:25 +0800

    add: 删除接口时同步更新proxy_server

--- app/router/interface_specific.js
+++ app/router/interface_specific.js
@@ -27,10 +27,9 @@ var handlers = {
         });
     },
     delete: function (req, res) {
-        interface.findById(req.params.id)
-            .then(function (result) {
-                result.destroy();
-            }).then(function () {
+        interface.destroy({ where: { id: req.params.id } })
+            .then(function () {
+                proxy_server.delInterface(req.params.id);
                 res.sendStatus(200);
             }).catch(function (err) {
                 console.warn(err);
