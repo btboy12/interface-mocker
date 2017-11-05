723d6cb4aa511f9be732e26889ec6f339596562e
btboy12 <btboy12@gmail.com>
2017-11-05 22:57:26 +0800

    mod: use async method to replace sync method in handle router

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParse = require('cookie-parser');
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
 const proxyServer = require("./app/proxy_server");
 const argv = require("argv");
 
@@ -34,22 +35,31 @@ var { options } = argv.option([
     }
 ]).run();
 
-for (var jade of fs.readdirSync(jade_path)) {
-    ((jade1) => {
-        if (jade1.indexOf('.') > 0) {
-            var j = jade1.split('.')[0]
-            app.get(`/${j}`, function (req, res) {
-                // res.sendFile(`${process.cwd()}/app/static/html/${url}.html`);
-                res.render(j);
-            });
+fs.readdir(jade_path, (err, files) => {
+    files.map(value => {
+        if (value.indexOf('.') > 0) {
+            var f_name = value.split('.')[0]
+            app.get(`/${f_name}`, (req, res) => { res.render(f_name) });
         }
-    })(jade);
-}
+    });
+});
+
+fs.readdir(router_path, (err, files) => {
+    files.map(value => {
+        if (value.indexOf(".") > 0) {
+            var router_module = require(path.join(router_path, file1));
+            var router = app.route(router_module.path);
+            for (var handler in router_module.handlers) {
+                router[handler](router_module.handlers[handler]);
+            }
+        }
+    })
+});
 
 for (var file of fs.readdirSync(router_path)) {
     ((file1) => {
         if (file1.indexOf(".") > 0) {
-            var router_module = require(router_path + file1);
+            var router_module = require(path.join(router_path, file1));
             var router = app.route(router_module.path);
             for (var handler in router_module.handlers) {
                 router[handler](router_module.handlers[handler]);
